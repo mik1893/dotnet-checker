6d9451f6a6018508427b8ff596591dac632bf490
mik1893 <michele.roscelli@gmail.com>
2018-08-15 13:36:50 +0100

    First working version

---
+++ DotNetChecker/DotNetCheck.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Threading.Tasks;
+
+namespace DotNetChecker
+{
+    public static class DotNetCheck
+    {
+
+
+        public static List<string> GetDotNetUpdateListNew()
+        {
+            List<string> updateList = new List<string>();
+
+            ManagementObjectCollection moReturn;
+            ManagementObjectSearcher moSearch;
+
+            moSearch = new ManagementObjectSearcher("Select * from Win32_Product");
+
+            moReturn = moSearch.Get();
+            foreach (ManagementObject mo in moReturn)
+            {
+                updateList.Add(mo["Name"].ToString());
+            }
+
+            return updateList;
+        }
+ 
+        public static List<string> GetDotNetUpdateList()
+        {
+            List<string> updateList = new List<string>();
+            
+            using (RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\Updates"))
+            {
+                foreach (string baseKeyName in baseKey.GetSubKeyNames())
+                {
+                    if (baseKeyName.Contains(".NET Framework 4 Extended"))
+                    {
+                        using (RegistryKey updateKey = baseKey.OpenSubKey(baseKeyName))
+                        {
+                            // Console.WriteLine(baseKeyName);
+                            foreach (string kbKeyName in updateKey.GetSubKeyNames())
+                            {
+                                using (RegistryKey kbKey = updateKey.OpenSubKey(kbKeyName))
+                                {
+                                    updateList.Add(baseKeyName+" - " + kbKeyName);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return updateList;
+        }
+
+        public static string GetDotNetVersion()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                if (ndpKey != null && ndpKey.GetValue("Release") != null)
+                {
+
+                    return CheckFor45PlusVersion((int)ndpKey.GetValue("Release"));
+                }
+                else
+                {
+                    return "No 4.5 or later version detected";
+                }
+            }
+        }
+
+        // Checking the version using >= will enable forward compatibility.
+        private static string CheckFor45PlusVersion(int releaseKey)
+        {
+            if (releaseKey >= 461808)
+                return "4.7.2 or later";
+            if (releaseKey >= 461308)
+                return "4.7.1";
+            if (releaseKey >= 460798)
+                return "4.7";
+            if (releaseKey >= 394802)
+                return "4.6.2";
+            if (releaseKey >= 394254)
+            {
+                return "4.6.1";
+            }
+            if (releaseKey >= 393295)
+            {
+                return "4.6";
+            }
+            if ((releaseKey >= 379893))
+            {
+                return "4.5.2";
+            }
+            if ((releaseKey >= 378675))
+            {
+                return "4.5.1";
+            }
+            if ((releaseKey >= 378389))
+            {
+                return "4.5";
+            }
+            // This code should never execute. A non-null release key should mean
+            // that 4.5 or later is installed.
+            return "No 4.5 or later version detected";
+        }
+    }
+}
--- DotNetChecker/MainWindow.xaml.cs
+++ DotNetChecker/MainWindow.xaml.cs
@@ -12,17 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace DotNetChecker
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            CheckDotNetVersion();
+        }
+
+        public void CheckDotNetVersion()
+        {
+            DotNetVersion.Content = DotNetCheck.GetDotNetVersion();
+            UpdateList.ItemsSource = DotNetCheck.GetDotNetUpdateList();
+        }
+
+        private void UpdateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string str = UpdateList.SelectedItem.ToString();
+            string kbnumber = str.Substring(str.LastIndexOf("KB") + 2);
+            System.Diagnostics.Process.Start("https://support.microsoft.com/kb/"+kbnumber);
         }
     }
 }
